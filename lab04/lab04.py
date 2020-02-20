5d8341ed998753ff8acf319895e123fa77174c37
buianh <buimanh1505@gmail.com>
2020-02-19 16:59:02 -0800

    lab4

---
+++ lab04/lab04.py
@@ -0,0 +1,158 @@
+""" Lab 04 """
+
+
+this_file = __file__
+
+def skip_add(n):
+    """ Takes a number n and returns n + n-2 + n-4 + n-6 + ... + 0.
+
+    >>> skip_add(5)  # 5 + 3 + 1 + 0
+    9
+    >>> skip_add(10) # 10 + 8 + 6 + 4 + 2 + 0
+    30
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'skip_add',
+    ...       ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 0:
+        return 0
+    else:
+        return skip_add(n-2) + n
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(1)
+    else:
+        return summation(n-1,term) + term(n)
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a < b:
+        return gcd(b,a)
+    elif a % b ==0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if m < 1 or n < 1:
+        return 0
+    elif m == 1 or n == 1:
+        return 1
+    else:
+        return paths(m-1,n) + paths(m,n-1) 
+
+
+def max_subseq(n, l):
+    """
+    Return the maximum subsequence of length at most l that can be found in the given number n.
+    For example, for n = 20125 and l = 3, we have that the subsequences are
+        2
+        0
+        1
+        2
+        5
+        20
+        21
+        22
+        25
+        01
+        02
+        05
+        12
+        15
+        25
+        201
+        202
+        205
+        212
+        215
+        225
+        012
+        015
+        025
+        125
+    and of these, the maxumum number is 225, so our answer is 225.
+
+    >>> max_subseq(20125, 3)
+    225
+    >>> max_subseq(20125, 5)
+    20125
+    >>> max_subseq(20125, 6) # note that 20125 == 020125
+    20125
+    >>> max_subseq(12345, 3)
+    345
+    >>> max_subseq(12345, 0) # 0 is of length 0
+    0
+    >>> max_subseq(12345, 1)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if l == 0:
+        return 0
+    elif l > len(str(n)):
+        length = len(str(n))
+    else:
+        length = l
+    list_return =[]
+    original_list = [int(x) for x in str(n)]
+    while length > 0:
+        new_list = original_list[0:len(original_list)-length+1]
+        max_new_list = max(new_list)
+        list_return.append(max_new_list)
+        max_new_list_index = original_list.index(max_new_list)
+        if max_new_list_index == 0:
+            original_list = original_list[1: len(original_list)]
+        else:
+            original_list = original_list[max_new_list_index: len(original_list)]
+        length -= 1
+    number = int(''.join(map(str,list_return)))
+    return number
